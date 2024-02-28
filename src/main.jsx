c95efe0172853dd8766a8273c8f821bfe88a8da5
Samyeol Son <samyeol0930@gmail.com>
2024-02-28 23:25:44 +0900

    [setup] tanstack query 사용 환경 구성

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
