c16571373c7671ecacf5f632a6483fd810b5e150
DragonTiger92 <dczwtu12b@gmail.com>
2024-03-04 02:21:01 +0900

    [Build] Storybook 삭제 후 재설치 및 addon 등 Storybook 관련 추가 패키지 설치

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      expanded: true,
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
