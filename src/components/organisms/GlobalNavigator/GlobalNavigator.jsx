97e0e14471192c619ae4572d97000e508c765169
Samyeol Son <samyeol0930@gmail.com>
2024-03-05 21:29:35 +0900

    [Style] GNB에 background color 설정

--- src/components/organisms/GlobalNavigator/GlobalNavigator.jsx
+++ src/components/organisms/GlobalNavigator/GlobalNavigator.jsx
@@ -14,7 +14,7 @@ export default function GlobalNavigator() {
   };
 
   return (
-    <nav className="fixed bottom-0 left-0 w-screen border-t border-primary-500 min-w-[320px]">
+    <nav className="fixed bottom-0 left-0 w-screen border-t border-primary-500 min-w-[320px] bg-grayscale-white">
       <ul
         onClick={handleClick}
         className="flex gap-4 justify-between items-center font-medium text-gray-400"
