c797a5ad964fb647c1f0d77d8dbfb348cc8d512f
Samyeol Son <samyeol0930@gmail.com>
2024-03-07 00:15:10 +0900

    [Modify] 이상 문자 제거

--- src/components/atoms/MemoDate/MemoDate.jsx
+++ src/components/atoms/MemoDate/MemoDate.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
-import convertDayFormat from '/src/utils/convertDayFormat'
+import convertDayFormat from '/src/utils/convertDayFormat';
 
 function MemoDate({ date }) {
   return (
     <span className="contents-xs text-gray-300 text-right">
-      {convertDayFormat(date)}
+      {convertDayFormat(date)}
     </span>
   );
 }
