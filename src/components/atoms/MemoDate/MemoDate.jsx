cfd172ab956b99a0e219db6666d08f5983dfa66e
Samyeol Son <samyeol0930@gmail.com>
2024-03-06 16:24:46 +0900

    [Feat] 메모 생성일 Formating

--- src/components/atoms/MemoDate/MemoDate.jsx
+++ src/components/atoms/MemoDate/MemoDate.jsx
@@ -1,7 +1,12 @@
 import PropTypes from 'prop-types';
+import { calcDate } from '../../../utils/calcDate';
 
 function MemoDate({ date }) {
-  return <span className="contents-xs text-gray-300 text-right">{date}</span>;
+  return (
+    <span className="contents-xs text-gray-300 text-right">
+      {calcDate(date)}
+    </span>
+  );
 }
 
 MemoDate.propTypes = { date: PropTypes.string.isRequired };
