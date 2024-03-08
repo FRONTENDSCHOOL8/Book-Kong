b00849e53bcdf10126cf3f9a3470dc3e4e18ec6e
yen815 <chldpdms815@gmail.com>
2024-03-08 18:19:05 +0900

    [Feture] 책나무 - 누적 페이지를 이용한 책 쌓기 기능 구현

--- src/components/pages/LibraryPage.jsx
+++ src/components/pages/LibraryPage.jsx
@@ -1,9 +1,12 @@
+// import BookBlockList from '../molecules/BookBlockList/BookBlockList';
+import BookTree from '../organisms/BookTree/BookTree';
 import LargeHeader from '/src/components/organisms/Header/LargeHeader/LargeHeader';
 
 export default function LibraryPage() {
   return (
     <>
       <LargeHeader title={'서재'} />
+      <BookTree />
     </>
   );
 }
