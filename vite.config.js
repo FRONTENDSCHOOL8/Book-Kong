28f13f2c3b84e7621aefa0c321184171267528b9
Samyeol Son <samyeol0930@gmail.com>
2024-03-08 20:34:27 +0900

    [BugFix] vite optimization error 해결

--- vite.config.js
+++ vite.config.js
@@ -13,46 +13,49 @@ const __dirname = path.resolve();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		viteImagemin({
-			plugins: {
-				jpg: imageminMozjpeg(),
-				png: imageminPngQuant(),
-				gif: imageminGifSicle(),
-				svg: imageminSvgo(),
-			},
-			makeWebp: {
-				plugins: {
-					jpg: imageminWebp(),
-					png: imageminWebp(),
-				},
-			},
-		}),
-	],
-	server: {
-		port: 3000, // default: 5173
-		open: true, // default: false
-	},
-	resolve: {
-		alias: { '@': path.resolve(__dirname, './src') },
-	},
-	css: {
-		devSourcemap: true,
-		modules: {
-			// generateScopedName: '[name]__[local]--[hash:base64:12]',
-		},
-	},
-	build: {
-		cssCodeSplit: true,
-		rollupOptions: {
-			output: {
-				manualChunks: {
-					react: ['react'],
-					reactDom: ['react-dom'],
-					reactRouter: ['react-router-dom'],
-				},
-			},
-		},
-	},
+  optimizeDeps: {
+    exclude: ['js-big-decimal'],
+  },
+  plugins: [
+    react(),
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+        png: imageminPngQuant(),
+        gif: imageminGifSicle(),
+        svg: imageminSvgo(),
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+          png: imageminWebp(),
+        },
+      },
+    }),
+  ],
+  server: {
+    port: 3000, // default: 5173
+    open: true, // default: false
+  },
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
+  css: {
+    devSourcemap: true,
+    modules: {
+      // generateScopedName: '[name]__[local]--[hash:base64:12]',
+    },
+  },
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react'],
+          reactDom: ['react-dom'],
+          reactRouter: ['react-router-dom'],
+        },
+      },
+    },
+  },
 });
