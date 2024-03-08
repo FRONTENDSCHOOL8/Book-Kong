ab795995358500f5e2c3a27a907a7a83eb58c5f6
Samyeol Son <samyeol0930@gmail.com>
2024-03-08 22:13:16 +0900

    [Test] 배포 테스트

--- src/api/searchAladin.js
+++ src/api/searchAladin.js
@@ -1,6 +1,3 @@
-const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
-const URL = `${PROXY}`;
-
 export async function getBookData(
   keyword = '',
   startPage = '1',
@@ -10,7 +7,7 @@ export async function getBookData(
 
   const bookData = await (
     await fetch(
-      `${URL}${import.meta.env.VITE_PB_SEARCH}/?Query=${keyword}&start=${startPage}&MaxResults=${maxResults}`
+      `${import.meta.env.VITE_PB_SEARCH}/?Query=${keyword}&start=${startPage}&MaxResults=${maxResults}`
     )
   ).json();
 
