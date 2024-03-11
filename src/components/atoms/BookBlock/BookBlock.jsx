ce4fc29cbe266836b19d81d44c9017669286bcec
yen815 <chldpdms815@gmail.com>
2024-03-11 11:35:11 +0900

    [BugFix] BookBlock(tailwind css) - height 값 적용 코드 변경 - height 값 동적 적용을 변수에 담아 활용 - height 값 prop 활용은 style로 적용 - style에 Math.max로 최소 px은 24로 고정

--- src/components/atoms/BookBlock/BookBlock.jsx
+++ src/components/atoms/BookBlock/BookBlock.jsx
@@ -1,35 +1,43 @@
 import { number, string } from 'prop-types';
 import BookTitle from '../../subatoms/BookTitle/BookTitle';
 
+// 페이지 별 키(key)에 설정할 높이를 아래처럼 작성합니다.
+
 function BookBlock({ title, page, index }) {
+  const pageMap = {
+    page: `h-[${Math.max(page * 0.1, 24)}px]`,
+  };
+
+  // 공통 클래스 이름
+  const commonClassNames =
+    'bookTree-list w-[230px] border rounded-sm border-grayscale-white display flex justify-center items-center';
+
+  // 병합할 클래스 이름 (공통 클래스 이름 + 페이지 맵의 클래스 이름)
+  let classNames = `${commonClassNames} ${pageMap[page]}`;
+
+  // 조건 처리
+  // 케이스 별 클래스 이름 병합
   switch (index % 3) {
     case 0:
-      return (
-        <li
-          className={`h-[${page}px] bookTree-list w-[230px] translate-x-[-24px] border rounded-sm border-grayscale-white bg-primary-500 text-white  display flex justify-center items-center`}
-        >
-          <BookTitle>{title}</BookTitle>
-        </li>
-      );
-
+      classNames += ' translate-x-[-24px] bg-primary-500 text-white';
+      break;
     case 1:
-      return (
-        <li
-          className={`h-[${page}px] bookTree-list w-[230px] translate-x-6 border rounded-sm border-grayscale-white bg-badge-yellow text-primary-500 display flex justify-center items-center`}
-        >
-          <BookTitle>{title}</BookTitle>
-        </li>
-      );
-
+      classNames += ' translate-x-6 bg-badge-yellow text-primary-500';
+      break;
     case 2:
-      return (
-        <li
-          className={`h-[${page}px] bookTree-list w-[230px] border rounded-sm border-grayscale-white bg-badge-yellow-02 text-grayscale-900  display flex justify-center items-center`}
-        >
-          <BookTitle>{title}</BookTitle>
-        </li>
-      );
+      classNames += ' bg-badge-yellow-02 text-grayscale-900';
+      break;
   }
+
+  // 마크업 반환 (병합된 클래스 이름 설정)
+  return (
+    <li
+      className={classNames}
+      style={{ height: `${Math.max(page * 0.1, 24)}px` }}
+    >
+      <BookTitle>{title}</BookTitle>
+    </li>
+  );
 }
 
 BookBlock.propTypes = {
