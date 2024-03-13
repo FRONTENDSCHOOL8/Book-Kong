348abf9ed45b83c350412310ef2761fd7299eb8e
Samyeol Son <samyeol0930@gmail.com>
2024-03-13 15:37:26 +0900

    [Feat] 책장 내 검색 기능 구현

--- src/components/molecules/SearchBar/SearchBar.jsx
+++ src/components/molecules/SearchBar/SearchBar.jsx
@@ -1,22 +1,28 @@
 import PropTypes from 'prop-types';
 import InputBar from '../../atoms/InputBar/InputBar';
 import SearchButton from '../../atoms/SearchButton/SearchButton';
+import { useState } from 'react';
 
-function SearchBar({ keyword, onChange, onSubmit }) {
+function SearchBar({ onSubmit }) {
+  const [keyword, setKeyword] = useState('');
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+    return;
+  };
+
   return (
     <form
       onSubmit={onSubmit}
       className="bg-primary-50 w-full flex justify-center items-center flex-shrink-0 rounded-[4px] border-[1px] border-primary-500 px-3 py-2 "
     >
-      <InputBar keyword={keyword} onChange={onChange} />
+      <InputBar keyword={keyword} onChange={handleChange} />
       <SearchButton />
     </form>
   );
 }
 
 SearchBar.propTypes = {
-  keyword: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
 };
 
--- src/components/organisms/Bookshelf/Bookshelf.jsx
+++ src/components/organisms/Bookshelf/Bookshelf.jsx
@@ -1,16 +1,23 @@
 import List from '../List/List';
 import { useQuery } from '@tanstack/react-query';
 import { loginUserData } from '../../../utils/controlUserData';
-import { getUserLibraryData } from '../../../utils/controlBookData';
+import { searchLibraryData } from '../../../utils/controlBookData';
 import { useState } from 'react';
 import Filter from '../../molecules/Filter/Filter';
+import SearchBar from '../../molecules/SearchBar/SearchBar';
+import { useEffect } from 'react';
 
 function Bookshelf() {
   const [filter, setFilter] = useState('전체');
-  const { data } = useQuery({
+  const [query, setQuery] = useState('');
+  const { data, refetch } = useQuery({
     queryKey: ['bookshelf', loginUserData.id],
-    queryFn: () => getUserLibraryData(),
+    queryFn: () => searchLibraryData(query),
   });
+
+  useEffect(() => {
+    refetch();
+  }, [query, refetch]);
 
   const handleClick = (e) => {
     const button = e.target.closest('button');
@@ -19,8 +26,15 @@ function Bookshelf() {
     setFilter(button.innerText);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newKeyword = e.target[0].value.trim();
+    setQuery(newKeyword);
+  };
+
   return (
     <main className="px-4 pt-[129px] pb-[120px] overflow-scroll h-screen">
+      <SearchBar onSubmit={handleSubmit} />
       <Filter onClick={handleClick} filter={filter} />
       <List
         data={
