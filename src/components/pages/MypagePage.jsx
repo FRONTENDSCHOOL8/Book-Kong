776a81fe5683f4afd5259b9e9deecfd7671c4eb7
Samuel Son <74633959+Samuel-0930@users.noreply.github.com>
2024-03-28 13:14:32 +0900

    변수명 변경

--- src/components/pages/MypagePage.jsx
+++ src/components/pages/MypagePage.jsx
@@ -13,8 +13,8 @@ export default function MypagePage() {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    const bool = clearLoginUserData();
-    if (bool) {
+    const isClearLoginData = clearLoginUserData();
+    if (isClearLoginData) {
       alert('로그아웃이 되었습니다.');
       // 로그아웃시 로그인페이지로 이동
       navigate('/login');
