5131d878f5fbb4ece44fe36dd57628c37718b8f2
Samyeol Son <samyeol0930@gmail.com>
2024-04-03 22:57:29 +0900

    [Temp] branch move error

---
+++ src/components/organisms/BookList/BookList.jsx
@@ -0,0 +1,32 @@
+import Card from '../../molecules/Card/Card';
+import { motion } from 'framer-motion';
+import PropTypes, { object } from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+function BookList({ data }) {
+  return (
+    <motion.ul
+      variants={ListVar}
+      initial="start"
+      animate="end"
+      className="flex flex-col gap-3"
+    >
+      {data?.map((book) => (
+        <Card key={uuidv4()} data={book} />
+      ))}
+    </motion.ul>
+  );
+}
+
+const ListVar = {
+  start: { opacity: 0 },
+  end: {
+    opacity: 1,
+    transition: { type: 'spring', mass: 0.8, staggerChildren: 0.05 },
+  },
+};
+
+BookList.propTypes = {
+  data: PropTypes.arrayOf(object),
+};
+
+export default BookList;
---
+++ src/components/organisms/BookList/BookList.stories.js
@@ -0,0 +1,15 @@
+import BookList from './BookList.jsx';
+
+const metaConfig = {
+  title: 'components/List',
+  component: BookList,
+  tags: ['autodocs'],
+  args: {},
+};
+
+export default metaConfig;
+
+export const Base = {
+  args: {},
+};
+Base.storyName = 'BookList';
