a2ab32bea737abbc00a2cc91b5c10545d5a900a9
Samyeol Son <samyeol0930@gmail.com>
2024-04-12 15:25:50 +0900

    [Refactor] Header Refactor

--- src/components/organisms/Header/Header/Header.jsx
+++ src/components/organisms/Header/Header/Header.jsx
@@ -4,49 +4,29 @@ import TabNavigation from '../../TabNavigation/TabNavigation';
 import SmallHeader from '../SmallHeader/SmallHeader';
 
 function Header({ title }) {
+  const titlesWithLargeHeader = [
+    '기록',
+    '서재',
+    '피드',
+    '캐릭터',
+    '마이페이지',
+  ];
+  const titlesWithSmallHeader = ['검색하기', '직접 입력하기', '상세페이지'];
+  const titlesWithTabNavigation = ['기록', '서재'];
+
   return (
     <header className="fixed top-0 z-20 left-[50%] translate-x-[-50%] w-full min-w-[320px] max-w-md">
-      {title === '기록' && (
+      {titlesWithLargeHeader.includes(title) && (
         <>
           <LargeHeader title={title} />
-          <TabNavigation page={title} />
+          {titlesWithTabNavigation.includes(title) && (
+            <TabNavigation page={title} />
+          )}
         </>
       )}
-      {title === '서재' && (
+      {titlesWithSmallHeader.includes(title) && (
         <>
-          <LargeHeader title={title} />
-          <TabNavigation page={title} />
-        </>
-      )}
-      {title === '피드' && (
-        <>
-          <LargeHeader title={title} />
-        </>
-      )}
-      {title === '캐릭터' && (
-        <>
-          <LargeHeader title={title} />
-        </>
-      )}
-      {title === '마이페이지' && (
-        <>
-          <LargeHeader title={title} />
-        </>
-      )}
-
-      {title === '검색하기' && (
-        <>
-          <SmallHeader title={title} />
-        </>
-      )}
-      {title === '직접 입력하기' && (
-        <>
-          <SmallHeader title={title} />
-        </>
-      )}
-      {title === '상세페이지' && (
-        <>
-          <SmallHeader title="" />
+          <SmallHeader title={title === '상세페이지' ? '' : title} />
         </>
       )}
     </header>
--- src/components/organisms/Header/SmallHeader/SmallHeader.jsx
+++ src/components/organisms/Header/SmallHeader/SmallHeader.jsx
@@ -7,26 +7,33 @@ import WriteMemoIconButton from '../../../atoms/WriteMemoIconButton/WriteMemoIco
 import SaveButton from '../../../atoms/SaveButton/SaveButton';
 
 function SmallHeader({ title }) {
+  // 버튼 그룹을 렌더링하는 함수를 정의
+  const renderButtons = () => {
+    switch (title) {
+      case '검색하기':
+        return <AddBookIconButton />;
+      case '메모':
+      case '피드':
+        return (
+          <div className="flex gap-5">
+            <DeleteIconButton />
+            <WriteMemoIconButton />
+          </div>
+        );
+      case '메모 작성':
+      case '피드 작성':
+      case '직접 입력하기':
+        return <SaveButton />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <header className="relative flex justify-between items-center px-4 py-[16px] border-b border-grayscale-200 bg-grayscale-white">
       <Back title={title} />
       <SmallHeaderTitle title={title} />
-      {title === '검색하기' && <AddBookIconButton />}
-      {title === '메모' && (
-        <div className="flex gap-5">
-          <DeleteIconButton />
-          <WriteMemoIconButton />
-        </div>
-      )}
-      {title === '메모 작성' && <SaveButton />}
-      {title === '피드' && (
-        <div className="flex gap-5">
-          <DeleteIconButton />
-          <WriteMemoIconButton />
-        </div>
-      )}
-      {title === '피드 작성' && <SaveButton />}
-      {title === '직접 입력하기' && <SaveButton />}
+      {renderButtons()}
     </header>
   );
 }
