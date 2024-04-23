8e3abc14f48102a16dd47623ce26c0f3f712ca30
juhyuny <juhyun5358@gmail.com>
2024-04-23 23:39:39 +0900

    [Feat] Splash 마크업, CSS 구현

--- src/components/pages/Splash.jsx
+++ src/components/pages/Splash.jsx
@@ -1,11 +1,11 @@
 
 const Splash = () => {
   return (
-    <div className="flex justify-center w-full h-screen z-9999 bg-primary-500">
+    <div className="flex flex-col items-center w-full h-screen  z-9999 bg-primary-500">
       <div className="w-[375px]">
-        <img src="/splash/splash-1.png" className="w-full translate-y-[-272px] animate-splash-1 splash-delay " alt="read" />
-        <img src="/splash/splash-2.png" className="w-full translate-y-[-500px] animate-splash-2 splash-delay " alt="and" />
-        <img src="/splash/splash-3.png" className="w-full translate-y-[-740px] animate-splash-3 splash-delay " alt="pile up" />
+        <img src="/splash/splash-1.png" className="w-full translate-y-[-272px] animate-splash-1 splash-delay animate-splash" alt="read" />
+        <img src="/splash/splash-2.png" className="w-full translate-y-[-500px] animate-splash-2 splash-delay animate-splash" alt="and" />
+        <img src="/splash/splash-3.png" className="w-full translate-y-[-740px] animate-splash-3 splash-delay animate-splash" alt="pile up" />
       </div>
       <h1 className="fixed bottom-1 left-[calc(50%-60px)]">
         <img src="/logo/logo-white.png" className="w-[120px]" alt="북콩 로고" />
