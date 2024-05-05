a52c9c18e4edf36afaffc17c1241f7b60fc66e3d
DragonTiger92 <dczwtu12b@gmail.com>
2024-05-06 02:50:16 +0900

    [Feat] MUI Skeleton 컴포넌트 root state 기본 스타일 변경 코드 작성

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,28 @@ import './styles/index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  components: {
+    MuiSkeleton: {
+      styleOverrides: {
+        root: {
+          maxWidth: '100%',
+        },
+      },
+    },
+  },
+});
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
