8a9171231343627b9a2c0380d6d1e03e0035cf62
DragonTiger92 <dczwtu12b@gmail.com>
2024-05-06 15:39:21 +0900

    [Remove] 필요 없는 스켈레톤 컴포넌트 파일 삭제

--- src/components/atoms/FilterIcon/FilterIconSkeleton.jsx
+++
@@ -1,7 +0,0 @@
-import Skeleton from '@mui/material/Skeleton';
-
-function FilterIconSkeleton() {
-  return <Skeleton className="w-12 h-[28px] rounded-[33px]" />;
-}
-
-export default FilterIconSkeleton;
--- src/components/molecules/SearchBar/SearchBarSkeleton.jsx
+++
@@ -1,7 +0,0 @@
-import Skeleton from '@mui/material/Skeleton';
-
-function SearchBarSkeleton() {
-  return <Skeleton variant="rounded" className="w-full rounded h-[36px]" />;
-}
-
-export default SearchBarSkeleton;
