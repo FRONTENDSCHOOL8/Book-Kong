2ec5542b9f617ab9ca89111729aee8092eb6c8ed
DragonTiger92 <dczwtu12b@gmail.com>
2024-05-06 15:43:58 +0900

    [Modify] theme -> muiTheme 변수명 변경

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import router from './Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider, createTheme } from '@mui/material';
 
-const theme = createTheme({
+const muiTheme = createTheme({
   components: {
     MuiSkeleton: {
       styleOverrides: {
@@ -22,7 +22,7 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={muiTheme}>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
