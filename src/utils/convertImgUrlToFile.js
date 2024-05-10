9e8f73ef213159610c12c54de324af6e1e06fe46
DragonTiger92 <dczwtu12b@gmail.com>
2024-05-10 15:43:47 +0900

    [Feat] Image URL을 File object로 변환하는 유틸 함수 생성

---
+++ src/utils/convertImgUrlToFile.js
@@ -0,0 +1,14 @@
+// 출처: https://velog.io/@ordidxzero/Image-URL%EC%9D%84-File-object%EB%A1%9C-%EB%B3%80%EA%B2%BD%ED%95%98%EA%B8%B0
+async function convertImgUrlToFile(url) {
+  const response = await fetch(url);
+  const data = await response.blob();
+  const ext = url.split('.').pop(); // url 구조에 맞게 수정할 것
+  const filename = url.split('/').pop(); // url 구조에 맞게 수정할 것
+  const metadata = { type: `image/${ext}` };
+
+  if (!filename) console.error('파일 이름이 제대로 추출 되지 않았습니다.');
+
+  return new File([data], filename, metadata);
+}
+
+export default convertImgUrlToFile;
