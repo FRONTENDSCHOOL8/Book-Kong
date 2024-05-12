d720bf9c756c208a528bd3dbaa682b66a9161578
DragonTiger92 <dczwtu12b@gmail.com>
2024-05-13 00:23:48 +0900

    [Feat] 알라딘 DB에서 요청하고자 하는 책 정보 불러오는 함수 작성 완료

--- src/api/searchAladin.js
+++ src/api/searchAladin.js
@@ -11,27 +11,16 @@ export async function getBookSearchData(keyword = '', pageParam) {
 }
 
 export async function getBookData(isbn13) {
-  const isbn13Raw = isbn13.slice(1, -2);
-
-  console.log(isbn13Raw);
-
   const isbn13Regex =
     /^(?:ISBN(?:-13)?:? )?(?<gs1>\d{3})(?:(?<number>\d{9})|(?=[\d -]{14}$)[ -](?<registrationGroup>\d{1,5})[ -](?<registrant>\d{1,7})[ -](?<publication>\d{1,6})[ -])(?<checkDigit>\d)$/gm;
 
-  if (isbn13Regex.test(isbn13Raw)) return;
+  if (!isbn13Regex.test(isbn13)) return;
 
   const bookQueryRes = await fetch(
-    `${import.meta.env.VITE_PB_BOOK_INFO}/?ItemId=${isbn13Raw}`
+    `${import.meta.env.VITE_PB_BOOK_INFO}/?ItemId=${isbn13}`
   );
+  const bookQueryJson = await bookQueryRes.json();
+  const bookData = bookQueryJson.response.json.item[0];
 
-  console.log(bookQueryRes);
-
-  // const bookQueryJson = await bookQueryRes.json();
-
-  // console.log(bookQueryJson);
-  // const bookInfo = bookQueryJson.response.json.item[0];
-
-  // return bookInfo;
-
-  return;
+  return bookData;
 }
