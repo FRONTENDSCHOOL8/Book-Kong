8d07ae628addccc90edf76330d37d89fcf14a95a
DragonTiger92 <dczwtu12b@gmail.com>
2024-05-14 21:51:53 +0900

    [Modify] 'tailwind.config.js'에서 plugin 불러오는 문법 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin');
+
+import plugin from 'tailwindcss/plugin';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
