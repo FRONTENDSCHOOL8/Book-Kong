9b51f0835f836868653b7041f78ac83e16690bf8
DragonTiger92 <dczwtu12b@gmail.com>
2024-05-20 13:57:56 +0900

    [Feat] 책 등록 페이지 내 '완독', '독서중', '희망 도서' 버튼 클릭 시 나타나는 ReadingStates 컴포넌트 내 마크업 전부 구현

--- src/components/molecules/ReadingStateButtons/ReadingStateButtons.jsx
+++ src/components/molecules/ReadingStateButtons/ReadingStateButtons.jsx
@@ -1,25 +1,25 @@
-import { memo, useState, useCallback } from 'react';
+import { memo } from 'react';
+import { string, func } from 'prop-types';
 import DoneButton from '../../atoms/ReadingStateButton/DoneButton/DoneButton';
 import WishButton from '../../atoms/ReadingStateButton/WishButton/WishButton';
 import ReadingButton from '../../atoms/ReadingStateButton/ReadingButton/ReadingButton';
 
-const ReadingStateButtons = memo(function ReadingStateButtons() {
-  const [status, setStatus] = useState('완독');
-
-  const handleChange = useCallback((e) => {
-    const input = e.target.closest('input');
-    if (!input) return;
-
-    setStatus(input.value);
-  }, []);
-
+const ReadingStateButtons = memo(function ReadingStateButtons({
+  status,
+  onChange,
+}) {
   return (
     <div className="flex justify-center gap-2 items-center">
-      <DoneButton onChange={handleChange} isChecked={status === '완독'} />
-      <ReadingButton onChange={handleChange} isChecked={status === '독서중'} />
-      <WishButton onChange={handleChange} isChecked={status === '희망 도서'} />
+      <DoneButton onChange={onChange} isChecked={status === '완독'} />
+      <ReadingButton onChange={onChange} isChecked={status === '독서중'} />
+      <WishButton onChange={onChange} isChecked={status === '희망 도서'} />
     </div>
   );
 });
+
+ReadingStateButtons.propTypes = {
+  status: string.isRequired,
+  onChange: func.isRequired,
+};
 
 export default ReadingStateButtons;
--- src/components/molecules/ReadingStates/ReadingStates.jsx
+++ src/components/molecules/ReadingStates/ReadingStates.jsx
@@ -1,14 +1,73 @@
+import Label from '../../atoms/Label/Label';
+import { useState, useCallback } from 'react';
+import PageReadBox from '../PageReadBox/PageReadBox';
+import DateInput from '../../atoms/DateInput/DateInput';
 import DateInputsBox from '../DateInputsBox/DateInputsBox';
-import ReviewContainer from '../ReviewContainer/ReviewContainer';
+import RatingContainer from '../RatingContainer/RatingContainer';
 import ReadingStateButtons from '../ReadingStateButtons/ReadingStateButtons';
+import ExpectTextareaBox from '../ExpectTextareaBox/ExpectTextareaBox';
 
 function ReadingStates() {
+  const [status, setStatus] = useState('완독');
+
+  const handleChange = useCallback((e) => {
+    const input = e.target.closest('input');
+
+    if (!input) return;
+
+    setStatus(input.value);
+  }, []);
+
+  switch (status) {
+    case '완독':
+      return (
+        <div>
+          <ReadingStateButtons status={status} onChange={handleChange} />
+          <DateInputsBox />
+          <RatingContainer label="평점" />
+        </div>
+      );
+
+    case '독서중':
+      return (
+        <div>
+          <ReadingStateButtons status={status} onChange={handleChange} />
+          <DateInputBox />
+          <PageReadBox />
+          <RatingContainer label="기대지수" />
+        </div>
+      );
+
+    case '희망 도서':
+      return (
+        <div>
+          <ReadingStateButtons status={status} onChange={handleChange} />
+          <ExpectTextareaBox />
+          <RatingContainer label="기대지수" />
+        </div>
+      );
+  }
+}
+
+function DateInputBox() {
+  const [startDate, setStartDate] = useState('');
+
+  const handleStartDate = useCallback((e) => {
+    setStartDate(e.target.value);
+  }, []);
+
   return (
-    <>
-      <ReadingStateButtons />
-      <DateInputsBox />
-      <ReviewContainer />
-    </>
+    <div className="flex items-center mt-6 mb-3">
+      <Label htmlFor="start-date" className="w-16 pr-2">
+        시작일
+      </Label>
+      <DateInput
+        id="start-date"
+        name="start_date"
+        value={startDate}
+        onChange={handleStartDate}
+      />
+    </div>
   );
 }
 
