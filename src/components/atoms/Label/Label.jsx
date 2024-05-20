a1c81fa9e543b9b7d08f8c63404f44574a6489e6
DragonTiger92 <dczwtu12b@gmail.com>
2024-05-20 12:05:11 +0900

    [BuxFix] Label 컴포넌트에 custom className 추가 시 custom className이 컴포넌트 기본 className보다 먼저 반영되지 않는 현상 해결

--- src/components/atoms/Label/Label.jsx
+++ src/components/atoms/Label/Label.jsx
@@ -1,5 +1,6 @@
 import { memo } from 'react';
 import { node, string } from 'prop-types';
+import getCombinedClassName from '../../../utils/getCombinedClassName';
 
 const Label = memo(function Label({
   htmlFor: id,
@@ -8,10 +9,21 @@ const Label = memo(function Label({
   ...restProps
 }) {
   const defaultClassName = 'contents-md-bold text-grayscale-900 w-full';
-  const combinedClassName = `${defaultClassName} ${customClassName}`.trim();
+
+  if (customClassName) {
+    return (
+      <label
+        htmlFor={id}
+        className={getCombinedClassName(defaultClassName, customClassName)}
+        {...restProps}
+      >
+        {children}
+      </label>
+    );
+  }
 
   return (
-    <label htmlFor={id} className={combinedClassName} {...restProps}>
+    <label htmlFor={id} className={defaultClassName} {...restProps}>
       {children}
     </label>
   );
