37fe4b3e0d751987895dc76bc7a235d1a19659ae
juhyuny <juhyun5358@gmail.com>
2024-06-04 17:23:59 +0900

    [Feat] DetailBookTitle에 BookName 불러오기

--- src/components/atoms/DetailBookName/DetailBookName.jsx
+++ src/components/atoms/DetailBookName/DetailBookName.jsx
@@ -1,11 +1,11 @@
-import { string, exact } from 'prop-types';
+import { string, shape } from 'prop-types';
 
 function DetailBookTitle({ data: bookData }) {
   return <p className="contents-md-bold">{bookData.title}</p>;
 }
 
 DetailBookTitle.propTypes = {
-  data: exact({
+  data: shape({
     title: string.isRequired,
   }),
 };
