426116d730fac37c6a7dbbabf31fc11fdb219549
juhyuny <juhyun5358@gmail.com>
2024-06-05 17:20:33 +0900

    [Feat] DetailBookStatePeriodContent에 데이터를  문자열로 나누어 년/월/일 표기

--- src/components/atoms/DetailBookStatePeriodContent/DetailBookStatePeriodContent.jsx
+++ src/components/atoms/DetailBookStatePeriodContent/DetailBookStatePeriodContent.jsx
@@ -1,13 +1,26 @@
-import {} from 'prop-types';
+import { string, shape } from 'prop-types';
 
-function DetailBookStatePeriodContent() {
+function DetailBookStatePeriodContent({ state: bookState }) {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    return `${year}년 ${month}월 ${day}일`;
+  };
   return (
-    <>
-      <span className="text-primary-500 contents-sm-bold">2023년 4월 5일</span>
-    </>
+    <span className="text-primary-500 contents-sm-bold">
+      {formatDate(bookState.start_date)}
+    </span>
   );
 }
 
-DetailBookStatePeriodContent.propTypes = {};
+DetailBookStatePeriodContent.propTypes = {
+  state: shape({
+    start_date: string.isRequired,
+  }),
+};
 
 export default DetailBookStatePeriodContent;
