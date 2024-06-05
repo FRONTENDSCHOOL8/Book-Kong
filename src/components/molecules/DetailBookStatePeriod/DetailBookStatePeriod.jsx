64340850e054562630f244328d3469b18a563c12
juhyuny <juhyun5358@gmail.com>
2024-06-05 17:23:32 +0900

    [Feat] DerailBookStatePeriod내부 컴포넌트에 bookState prop 넘기기

--- src/components/molecules/DetailBookStatePeriod/DetailBookStatePeriod.jsx
+++ src/components/molecules/DetailBookStatePeriod/DetailBookStatePeriod.jsx
@@ -1,16 +1,20 @@
-import {} from "prop-types";
-import DetailBookStateTitle from "../../atoms/DetailBookStateTitle/DetailBookStateTitle";
-import DetailBookStatePeriodContent from "../../atoms/DetailBookStatePeriodContent/DetailBookStatePeriodContent";
+import { string, shape } from 'prop-types';
+import DetailBookStateTitle from '../../atoms/DetailBookStateTitle/DetailBookStateTitle';
+import DetailBookStatePeriodContent from '../../atoms/DetailBookStatePeriodContent/DetailBookStatePeriodContent';
 
-function DetailBookStatePeriod() {
+function DetailBookStatePeriod({ state: bookState }) {
   return (
     <div className="flex flex-col items-center gap-3 w-[33.3333%] border-l border-grayscale-100">
-      <DetailBookStatePeriodContent />
+      <DetailBookStatePeriodContent state={bookState} />
       <DetailBookStateTitle title="완독일" />
     </div>
   );
 }
 
-DetailBookStatePeriod.propTypes = {  };
+DetailBookStatePeriod.propTypes = {
+  state: shape({
+    start_date: string,
+  }),
+};
 
 export default DetailBookStatePeriod;
