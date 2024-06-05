050d1127fd8a6f7860dbe6fbb13768d87d30c2dc
juhyuny <juhyun5358@gmail.com>
2024-06-05 22:44:58 +0900

    [Feat] 렌더링 될 MemoList, FeedList가 없을 경우 나타나는 예외상황 컴포넌트 생성

---
+++ src/components/molecules/NoneCardState/NoneCardState.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+function NoneCardState({ data }) {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    if (data === 'memo') {
+      setMessage('메모');
+    } else if (data === 'feed') {
+      setMessage('피드');
+    }
+  }, [data]);
+
+  return (
+    <div className="w-full py-[32px] flex flex-col gap-4 items-center text-center contents-md">
+      <img src="/images/characters/잠금캐릭.png" alt="" width="82" />
+      <p>
+        등록된 {message}가 없습니다. <br />
+        {message}를 등록해보세요!
+      </p>
+    </div>
+  );
+}
+
+NoneCardState.propTypes = {
+  data: PropTypes.string.isRequired,
+};
+
+export default NoneCardState;
