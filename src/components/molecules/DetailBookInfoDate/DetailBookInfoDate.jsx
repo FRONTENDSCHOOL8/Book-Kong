57428354d4b2b0e43f6af6bbe7137ba2fdd216cf
juhyuny <juhyun5358@gmail.com>
2024-06-05 16:34:18 +0900

    [Modify] DetailBookInfo에 publicationDate 데이터 불러오기 및 컴포넌트 분리

---
+++ src/components/molecules/DetailBookInfoDate/DetailBookInfoDate.jsx
@@ -0,0 +1,30 @@
+import { string, shape } from 'prop-types';
+
+function DetailBookInfoDate({ data: bookData }) {
+  const calculatePublicationDate = (publicDate) => {
+    const date = new Date(publicDate);
+
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+    const day = String(date.getUTCDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+  };
+
+  const pulicationDate = calculatePublicationDate(bookData.publication_date);
+
+  return (
+    <>
+      <span className="block mt-12 contents-md text-grayscale-400">출간일</span>
+      <p className="mt-4 contents-sm">{pulicationDate}</p>
+    </>
+  );
+}
+
+DetailBookInfoDate.propTypes = {
+  data: shape({
+    description: string.isRequired,
+  }),
+};
+
+export default DetailBookInfoDate;
