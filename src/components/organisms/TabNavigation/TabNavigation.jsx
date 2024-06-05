d90ca45f6ff08d6bfed381042ab8edf2adb19669
juhyuny <juhyun5358@gmail.com>
2024-06-06 00:08:30 +0900

    [Modify] DetailBook에서 썼던 Navigation 삭제(BookDetailNavigation으로 분리)

--- src/components/organisms/TabNavigation/TabNavigation.jsx
+++ src/components/organisms/TabNavigation/TabNavigation.jsx
@@ -1,29 +1,7 @@
 import PropTypes from 'prop-types';
 import TabButton from '../../atoms/TabButton/TabButton';
-import TabLink from '../../atoms/TabLink/TabLink';
-import { useState } from 'react';
 
 function TabNavigation({ page }) {
-  // 네비게이션 정보는 되도록 주소 입력창에 저장하는 것이 좋습니다.
-  //
-  const [button, setButton] = useState('책 정보');
-
-  const handleClick = (e) => {
-    const a = e.target.closest('a');
-    if (!a) return;
-
-    const target = a.innerText;
-
-    if (target === '책 정보') {
-      setButton('책 정보');
-    } else if (target === '메모') {
-      setButton('메모');
-    } else {
-      setButton('피드');
-    }
-
-    // 클릭할 때마다 버튼의 상태를 콘솔에 출력
-  }
   return (
     <>
       {page === '기록' && (
@@ -46,21 +24,6 @@ function TabNavigation({ page }) {
             </li>
             <li>
               <TabButton path={'/library/bookshelf'} title={'책장'} />
-            </li>
-          </ul>
-        </nav>
-      )}
-      {page === '책 상세' && (
-        <nav onClick={handleClick} className="sticky px-4 bg-background-gray left-0 top-[57px] z-20">
-          <ul className="flex items-center justify-start h-[54px]">
-            <li className="w-[33.3333%] h-full">
-              <TabLink path={'#bookInfo'} title={'책 정보'} button={button} />
-            </li>
-            <li className="w-[33.3333%] h-full">
-              <TabLink path={'#memo'} title={'메모'} button={button} />
-            </li>
-            <li className="w-[33.3333%] h-full">
-              <TabLink path={'#feed'} title={'피드'} button={button} />
             </li>
           </ul>
         </nav>
