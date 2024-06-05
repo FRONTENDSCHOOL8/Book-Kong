4966b9c8876f59c7237826da5b29c640e9949383
juhyuny <juhyun5358@gmail.com>
2024-06-05 17:25:49 +0900

    [Design] DetailBookMemo 레이아웃 css 변경 및 WriteMemoIconButton 컴포넌트 추가

--- src/components/molecules/DetailBookMemo/DetailBookMemo.jsx
+++ src/components/molecules/DetailBookMemo/DetailBookMemo.jsx
@@ -1,4 +1,3 @@
-// import PropTypes from "prop-types";
 import { useQuery } from '@tanstack/react-query';
 import OrderButton from '../OrderButton/OrderButton';
 import MemoCard from '../../organisms/MemoCard/MemoCard';
@@ -7,6 +6,7 @@ import { getUserMemoData } from '../../../utils/controlMemoData';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { motion } from 'framer-motion';
+import WriteMemoIconButton from '../../atoms/WriteMemoIconButton/WriteMemoIconButton';
 
 function DetailBookMemo() {
   const initialOrder = localStorage.getItem('order') || '최신순';
@@ -50,47 +50,55 @@ function DetailBookMemo() {
   };
 
   const [listCount, setListCount] = useState(0);
-  // const dataList = []; // 여기에 리스트 데이터가 들어간다고 가정합니다.
 
   useEffect(() => {
     setListCount(data?.length || 0);
   }, [data]);
 
   return (
-    <div
-      className="pb-8 bg-grayscale-white before:content-[''] before:block before:h-3 before:border-t before:border-b before:border-grayscale-100 before:mb-10 before:bg-background-gray"
+    <section
+      className="before:border-b before:content-[''] before:block before:pt-[111px] mt-[-111px] border-b"
       id="memo"
     >
-      <div className="flex gap-2 mx-4">
-        <h4 className="contents-lg-bold text-grayscale-400">메모</h4>
-        <span className="font-bold align-middle text-primary-500 contents-lg-bold">
-          {listCount}
-        </span>
+      <div className="pb-8 bg-grayscale-white ">
+        <div className="flex items-center justify-between pt-8 mx-4">
+          <div className="flex gap-2">
+            <h4 className="contents-lg-bold text-grayscale-900">메모</h4>
+            <span className="font-bold align-middle text-primary-500 contents-lg-bold">
+              {listCount}
+            </span>
+          </div>
+          <a
+            href="/record/memo"
+            className="text-grayscale-500 contents-sm-bold"
+          >
+            전체보기
+          </a>
+        </div>
+        <div className="flex flex-col items-end px-4 pb-[56px] ">
+          <div className="flex items-center justify-between w-full mt-6 mb-4">
+            <OrderButton onClick={toggleSort} order={order} />
+            <WriteMemoIconButton />
+          </div>
+          <motion.ul
+            variants={listVar}
+            initial="start"
+            animate="end"
+            className="flex flex-col w-full gap-3"
+          >
+            {data?.map((memo) => (
+              <MemoCard
+                key={memo.id}
+                title={memo.expand.book_id.title}
+                contents={memo.content}
+                date={memo.created}
+              />
+            ))}
+          </motion.ul>
+        </div>
       </div>
-      <motion.main className="flex flex-col items-end px-4 pb-[56px] ">
-        <OrderButton onClick={toggleSort} order={order} />
-        <motion.ul
-          variants={listVar}
-          initial="start"
-          animate="end"
-          className="flex flex-col w-full gap-3"
-        >
-          {data?.map((memo) => (
-            <MemoCard
-              key={memo.id}
-              title={memo.expand.book_id.title}
-              contents={memo.content}
-              date={memo.created}
-            />
-          ))}
-        </motion.ul>
-      </motion.main>
-    </div>
+    </section>
   );
 }
-
-// DetailBookMemo.propTypes = {
-
-//  };
 
 export default DetailBookMemo;
