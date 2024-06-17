70491e1f42c8922234b3d51978c3b2082bf0a787
juhyuny <juhyun5358@gmail.com>
2024-06-17 17:10:44 +0900

    [Mofidy] DetailBookImage 스토리북 파일 추가

---
+++ src/components/atoms/DetailBookImage/DetailBookImage.stories.js
@@ -0,0 +1,15 @@
+import DetailBookImage from './DetailBookImage';
+
+const metaConfig = {
+  title: 'components/DetailBookImage',
+  component: DetailBookImage,
+  tags: ['autodocs'],
+  args: {},
+};
+
+export default metaConfig;
+
+export const Base = {
+  args: {},
+};
+Base.storyName = 'DetailBookImage';
