ee4777dd88e28ca9e62fb7d8fa8041078b1537d3
juhyuny <juhyun5358@gmail.com>
2024-06-17 17:32:46 +0900

    [Modify] 불필요한 함수 삭제 및 유틸함수 사용

--- src/components/molecules/DetailBookInfoDate/DetailBookInfoDate.jsx
+++ src/components/molecules/DetailBookInfoDate/DetailBookInfoDate.jsx
@@ -1,31 +1,22 @@
 import { string, shape } from 'prop-types';
+import convertDayFormat from '../../../utils/convertDayFormat';
 
 function DetailBookInfoDate({ data: bookData }) {
-  const calculatePublicationDate = (publicDate) => {
-    const date = new Date(publicDate);
-
-    const year = date.getUTCFullYear();
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-    const day = String(date.getUTCDate()).padStart(2, '0');
-
-    return `${year}-${month}-${day}`;
-  };
-
-  const pulicationDate = calculatePublicationDate(bookData.publication_date);
-
   return (
     <>
       <span className="block mt-12 contents-md-bold text-grayscale-900">
         출간일
       </span>
-      <p className="mt-4 contents-sm">{pulicationDate}</p>
+      <p className="mt-4 contents-sm">
+        {convertDayFormat(bookData.publication_date)}
+      </p>
     </>
   );
 }
 
 DetailBookInfoDate.propTypes = {
   data: shape({
-    description: string.isRequired,
+    publication_date: string.isRequired,
   }),
 };
 
