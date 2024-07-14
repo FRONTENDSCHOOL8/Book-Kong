fef3911a352c4c825ceb80cab813f2ed83e4d798
juhyuny <juhyun5358@gmail.com>
2024-07-14 13:11:08 +0900

    [Feat] 메모등록페이지 마크업 및 스타일링 외

---
+++ src/components/pages/MemoRegistrationPage/MemoRegistrationPage.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import Header from '../../organisms/Header/Header/Header';
+import { Helmet } from 'react-helmet-async';
+
+function MemoRegistrationPage() {
+  const [text, setText] = useState('');
+
+  const handleTextCount = (e) => {
+    const newText = e.target.value;
+    if (newText.length <= 1000) {
+      setText(newText);
+    }
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>책콩 | 메모</title>
+      </Helmet>
+      <Header title="메모 작성" formId="memo-data" />
+      <main className="h-screen pb-[63px] pt-[65px]">
+        <article className="h-full border-y bg-grayscale-white border-1 border-grayscale-200">
+          <h2 className="sr-only">메모 작성</h2>
+          <form action="get" id="memo-data">
+            <div className="relative w-full h-auto pt-6 pb-4 pl-10 pr-4 leading-4 border-b border-grayscale-200 memo-icon text-primary-500">
+              <label htmlFor="MemoBookSelect" className="sr-only">
+                등록할 메모의 책 선택
+              </label>
+              <select
+                id="MemoBookSelect"
+                className="block w-full outline-none contents-sm-bold"
+              >
+                <option disabled selected className="default-option" value="">
+                  어떤 책에 대한 메모인가요?
+                </option>
+                <option value="">
+                  데일카네기 인간관계론 : 인간관계 바이블(원본 번역판)
+                </option>
+                <option value="">총 균 쇠</option>
+                <option value="">사랑의 기술</option>
+                <option value="">오만과 편견</option>
+                <option value="">사피엔스</option>
+              </select>
+            </div>
+            <div className="flex flex-col items-end px-4 pt-3">
+              <p className="text-grayscale-400">
+                <span
+                  className={`${
+                    text.length >= 1000
+                      ? 'text-primary-500'
+                      : 'text-grayscale-black'
+                  } contents-md-bold`}
+                >
+                  {text.length}
+                </span>{' '}
+                / 1000
+              </p>
+              <label htmlFor="MemoTextarea" className="sr-only">
+                책의 메모 입력
+              </label>
+              <textarea
+                className="w-full mt-[6px] h-[524px] block resize-none focus:outline-none"
+                onChange={handleTextCount}
+                value={text}
+                // name=""
+                id="MemoTextarea"
+                placeholder="메모를 입력해보세요!"
+              ></textarea>
+            </div>
+          </form>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export default MemoRegistrationPage;
