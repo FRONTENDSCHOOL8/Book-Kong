0f6e29ca32bc2d812a7dd7effba624b61aabde46
juhyuny <juhyun5358@gmail.com>
2024-08-20 21:56:01 +0900

    [Modify] 메모등록페이지 label 요소의 htmlFor, id 네이밍 kebab-case로 수정 및 name 추가

--- src/components/pages/MemoRegistrationPage/MemoRegistrationPage.jsx
+++ src/components/pages/MemoRegistrationPage/MemoRegistrationPage.jsx
@@ -21,26 +21,27 @@ function MemoRegistrationPage() {
       <Header title="메모 작성" formId="memo-data" />
       <main className="h-screen pb-[63px] pt-[65px]">
         <article className="h-full border-y bg-grayscale-white border-1 border-grayscale-200">
-          <A11yHidden as="h2">메모 작성하기</A11yHidden>
+          <A11yHidden as="h2">메모 작성</A11yHidden>
           <form action="get" id="memo-data">
             <div className="relative w-full h-auto pt-6 pb-4 pl-10 pr-4 leading-4 border-b border-grayscale-200 memo-icon text-primary-500">
               <A11yHidden as="label" htmlFor="memo-book-select">
                 등록할 메모의 책 선택
               </A11yHidden>
               <select
-                id="MemoBookSelect"
+                id="memo-book-select"
+                name="book-title"
                 className="block w-full outline-none contents-sm-bold"
               >
                 <option disabled selected className="default-option" value="">
                   어떤 책에 대한 메모인가요?
                 </option>
-                <option value="">
+                <option value="데일카네기 인간관계론 : 인간관계 바이블(원본 번역판)">
                   데일카네기 인간관계론 : 인간관계 바이블(원본 번역판)
                 </option>
-                <option value="">총 균 쇠</option>
-                <option value="">사랑의 기술</option>
-                <option value="">오만과 편견</option>
-                <option value="">사피엔스</option>
+                <option value="총 균 쇠">총 균 쇠</option>
+                <option value="사랑의 기술">사랑의 기술</option>
+                <option value="오만과 편견">오만과 편견</option>
+                <option value="사피엔스">사피엔스</option>
               </select>
             </div>
             <div className="flex flex-col items-end px-4 pt-3">
@@ -63,8 +64,8 @@ function MemoRegistrationPage() {
                 className="w-full mt-[6px] h-[524px] block resize-none focus:outline-none"
                 onChange={handleText}
                 value={text}
-                // name=""
-                id="MemoTextarea"
+                name="memo-contents"
+                id="memo-textarea"
                 placeholder="메모를 입력해보세요!"
               ></textarea>
             </div>
