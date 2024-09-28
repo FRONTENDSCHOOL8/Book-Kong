b74c2e4ff5e15822adc63703c77a4923eac22cc0
DragonTiger92 <dczwtu12b@gmail.com>
2024-09-28 23:43:46 +0900

    [Modify] BookInfoBox 컴포넌트 최상단 요소 <fieldset>으로 변경 및 하부에 legend 요소 추가

--- src/components/organisms/BookInfoBox/BookInfoBox.jsx
+++ src/components/organisms/BookInfoBox/BookInfoBox.jsx
@@ -3,6 +3,7 @@ import { useState, useCallback } from 'react';
 import debounce from '../../../utils/debounce';
 import TextInputLi from '../../molecules/TextInputLi/TextInputLi';
 import BookCoverInput from '../../atoms/BookCoverInput/BookCoverInput';
+import A11yHidden from '../../atoms/A11yHidden/A11yHidden';
 
 function BookInfoBox({ data: aladinBook }) {
   const [bookInfo, setBookInfo] = useState({
@@ -25,44 +26,47 @@ function BookInfoBox({ data: aladinBook }) {
   );
 
   return (
-    <ul className="flex flex-col gap-6">
-      <BookCoverInput title={bookInfo.title} cover={aladinBook?.cover} />
-      <TextInputLi
-        id="title"
-        label="책 제목"
-        name="title"
-        value={bookInfo.title}
-        onChange={handleChange}
-      />
-      <TextInputLi
-        id="author"
-        label="지은이/옮긴이"
-        name="author"
-        value={bookInfo.author}
-        onChange={handleChange}
-      />
-      <TextInputLi
-        id="publisher"
-        label="출판사"
-        name="publisher"
-        value={bookInfo.publisher}
-        onChange={handleChange}
-      />
-      <TextInputLi
-        id="page"
-        label="총 페이지"
-        name="total_page"
-        value={bookInfo.page}
-        onChange={handleChange}
-      />
-      <TextInputLi
-        id="isbn"
-        label="ISBN"
-        name="isbn_13"
-        value={bookInfo.isbn}
-        onChange={handleChange}
-      />
-    </ul>
+    <fieldset>
+      <A11yHidden as="legend">책 정보 입력</A11yHidden>
+      <ul className="flex flex-col gap-6">
+        <BookCoverInput title={bookInfo.title} cover={aladinBook?.cover} />
+        <TextInputLi
+          id="title"
+          label="책 제목"
+          name="title"
+          value={bookInfo.title}
+          onChange={handleChange}
+        />
+        <TextInputLi
+          id="author"
+          label="지은이/옮긴이"
+          name="author"
+          value={bookInfo.author}
+          onChange={handleChange}
+        />
+        <TextInputLi
+          id="publisher"
+          label="출판사"
+          name="publisher"
+          value={bookInfo.publisher}
+          onChange={handleChange}
+        />
+        <TextInputLi
+          id="page"
+          label="총 페이지"
+          name="total_page"
+          value={bookInfo.page}
+          onChange={handleChange}
+        />
+        <TextInputLi
+          id="isbn"
+          label="ISBN"
+          name="isbn_13"
+          value={bookInfo.isbn}
+          onChange={handleChange}
+        />
+      </ul>
+    </fieldset>
   );
 }
 
