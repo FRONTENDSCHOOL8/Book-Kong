7d90f405fbf2407f69f0a7556eb173a135ac64a0
DragonTiger92 <dczwtu12b@gmail.com>
2024-10-23 16:53:02 +0900

    [Modify] controlMemoData.js 내 함수의 정의 방식 변경 및 함수 설명 JSDoc 추가 외

--- src/utils/controlMemoData.js
+++ src/utils/controlMemoData.js
@@ -1,15 +1,19 @@
 import pb from '../api/pocketbase';
 import { loginUserData } from './controlUserData';
 
-export async function getUserMemoData(sort = '-created') {
-  return await pb
-    .collection('memos')
-    .getFullList({
-      filter: `user_id = '${loginUserData.id}'`,
-      sort: `${sort}`,
-    });
-}
+export const getUserMemoRecs = async (sort = '-created') =>
+  await pb.collection('memos').getFullList({
+    filter: `user_id = '${loginUserData.id}'`,
+    sort: `${sort}`,
+  });
 
-export async function getMemoData(memoId, options) {
-  return await pb.collection('memos').getOne(memoId, options);
-}
+/**
+ * 'memos' collection에서 하나의 record를 불러오는 함수
+ * @param { string } recId 'memos' collection record ID
+ * @param { Object } [options] PocketBase SDK 내 read를 위한 query options
+ * @prop { string } [expand] [PocketBase 공식 문서](https://pocketbase.io/docs/) 참고
+ * @prop { string } [fields] [PocketBase 공식 문서](https://pocketbase.io/docs/) 참고
+ * @returns 'memos' collection의 record 객체
+ */
+export const getOneMemoRec = async (recId, options) =>
+  await pb.collection('memos').getOne(recId, options);
