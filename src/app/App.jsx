742779643c02cb1179884be748fc5a0d80c46dbc
DragonTiger92 <dczwtu12b@gmail.com>
2024-10-28 01:15:17 +0900

    [BugFix] 6d37da82d606c846fc59bf579a8731718125fffc 내 SplashPage 렌더링 로직 수정 시, 잘못 작성한 코드로 인해 앱 접근 시 SplashPage가 제대로 렌더링 되지 않던 오류 수정

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import SplashPage from '../components/pages/SplashPage/SplashPage';
 import GlobalNavigator from '/src/components/organisms/GlobalNavigator/GlobalNavigator';
+import { loginUserData } from '../utils/controlUserData';
 
 function App() {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
   const [isSplashed, setIsSplashed] = useState(false);
 
-  if (!isSplashed) {
-    setIsSplashed(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsSplashed(true);
 
-    return <SplashPage />;
-  }
+      if (loginUserData.id) {
+        navigate('/library/booktree');
+      } else {
+        navigate('/login');
+      }
+
+      return () => clearTimeout(timer);
+    }, 3000);
+  });
+
+  if (!isSplashed) return <SplashPage />;
 
   if (pathname === ('/login' || '/register')) return <Outlet />;
 
