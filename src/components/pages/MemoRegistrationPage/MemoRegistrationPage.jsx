c76e8c474a822846e7d183df587ae0ac5d57e878
juhyuny <juhyun5358@gmail.com>
2024-10-27 14:19:11 +0900

    [Feat] 메모 등록 기능 구현 및 완료 시 페이지 이동

--- src/components/pages/MemoRegistrationPage/MemoRegistrationPage.jsx
+++ src/components/pages/MemoRegistrationPage/MemoRegistrationPage.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react';
-import Header from '../../organisms/Header/Header/Header';
+import { useState, useRef } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import Header from '../../organisms/Header/Header/Header';
 import A11yHidden from '../../atoms/A11yHidden/A11yHidden';
-import { useQuery } from '@tanstack/react-query';
+import {
+  // clearLoginUserData,
+  loginUserData,
+} from '../../../utils/controlUserData';
 import { getUserLibraryData } from '../../../utils/controlBookData';
-import { loginUserData } from '../../../utils/controlUserData';
+import pb from '../../../api/pocketbase';
+import { useNavigate } from 'react-router-dom';
 
 function MemoRegistrationPage() {
+  const navigate = useNavigate();
   const [text, setText] = useState('');
+  const formRef = useRef(null);
+  const [selectedBook, setSelectedBook] = useState('');
+
+  const handleSelect = (e) => {
+    setSelectedBook(e.target.value);
+    console.log(e.target.value);
+  };
 
   const handleText = (e) => {
     const newText = e.target.value;
@@ -22,6 +35,57 @@ function MemoRegistrationPage() {
     queryFn: async () => getUserLibraryData(),
   });
 
+  // 메모 생성 패치 함수
+  const createMemo = async (formData) => {
+    await pb.collection('memos').create(formData);
+  };
+
+  // 데이터 생성
+  const memoMutation = useMutation({
+    mutationFn: createMemo,
+    onSuccess: () => {
+      navigate('/record/memo');
+    },
+  });
+
+  //   const data = Object.fromEntries(formData.entries());  // 폼 데이터를 객체로 변환
+
+  // 폼 태그 submit
+  // const handleMemoSubmit = (e) => {
+  //   e.preventDafault();
+  //   console.log(formRef.current);
+
+  //   if (formRef.current) {
+  //     const formData = Object.fromgEntries(
+  //       new FormData(formRef.current).entries()
+  //     );
+  //     formData.book_id = selectState;
+  //     if (formData.book_id === '') {
+  //       throw new Error('책을 선택해주세요.');
+  //     }
+
+  //     // memoMutation.mutate(formData);
+  //   }
+  // };
+  // 폼 태그 Submit 작동 Action
+  const handleMemoSubmit = (e) => {
+    e.preventDefault();
+
+    if (formRef.current) {
+      const formData = new FormData(formRef.current);
+      new FormData(formRef.current).entries();
+
+      if (formData.get('book_title') === '') {
+        throw new Error('책을 선택해주세요.');
+      }
+
+      console.log(formRef.current);
+      formData.append('user_id', loginUserData.id);
+
+      memoMutation.mutate(formData);
+    }
+  };
+
   return (
     <>
       <Helmet>
@@ -31,17 +95,25 @@ function MemoRegistrationPage() {
       <main className="h-screen pb-[63px] pt-[65px]">
         <article className="h-full border-y bg-grayscale-white border-1 border-grayscale-200">
           <A11yHidden as="h2">메모 작성</A11yHidden>
-          <form action="get" id="memo-data">
+          <form
+            method="get"
+            className="h-full"
+            id="memo-data"
+            ref={formRef}
+            onSubmit={handleMemoSubmit}
+          >
             <div className="relative w-full h-auto pt-6 pb-4 pl-10 pr-4 leading-4 border-b border-grayscale-200 memo-icon text-primary-500">
               <A11yHidden as="label" htmlFor="memo-book-select">
                 등록할 메모의 책 선택
               </A11yHidden>
               <select
                 id="memo-book-select"
-                name="book-title"
-                className="block w-full outline-none contents-sm-bold"
+                name="book_title"
+                className="block w-full outline-none cursor-pointer contents-sm-bold"
+                onChange={handleSelect}
+                value={selectedBook}
               >
-                <option disabled selected className="default-option" value="">
+                <option disabled className="default-option" value="">
                   어떤 책에 대한 메모인가요?
                 </option>
                 {librariesData?.map((libRecord) => (
@@ -51,7 +123,7 @@ function MemoRegistrationPage() {
                 ))}
               </select>
             </div>
-            <div className="flex flex-col items-end px-4 pt-3">
+            <div className="flex flex-col items-end h-full pb-[79px] px-4 pt-3">
               <p className="text-grayscale-400">
                 <span
                   className={`${
@@ -68,10 +140,10 @@ function MemoRegistrationPage() {
                 책의 메모 입력
               </A11yHidden>
               <textarea
-                className="w-full mt-[6px] h-[524px] block resize-none focus:outline-none"
+                className="w-full h-full mt-[6px] block resize-none focus:outline-none custom-scroll-hidden"
                 onChange={handleText}
                 value={text}
-                name="memo-contents"
+                name="content"
                 id="memo-textarea"
                 placeholder="메모를 입력해보세요!"
               ></textarea>
