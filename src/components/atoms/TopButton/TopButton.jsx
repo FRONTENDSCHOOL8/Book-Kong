a4925af71e1eb786fec28f288e95744899978f69
juhyuny <juhyun5358@gmail.com>
2024-10-29 02:35:03 +0900

    [Design] TopButton 마크업 및 스타일 수정

--- src/components/atoms/TopButton/TopButton.jsx
+++ src/components/atoms/TopButton/TopButton.jsx
@@ -2,13 +2,16 @@ import {} from 'prop-types';
 
 function TopButton() {
   return (
-    <button
-      type="button"
-      aria-label="웹페이지의 맨위로 이동하기"
-      className="fixed right-4 bottom-[66px]"
-    >
-      <img className="w-12 h-12" src="/images/icons/top-button.svg" alt="" />
-    </button>
+    // 웹 브라우저에서 위치조정을 위해 컨테이너 추가
+    <div className="max-w-[448px] w-full min-w-[320px] fixed left-[50%] -translate-x-1/2 bottom-0 bg-pink-400 h-5">
+      <button
+        type="button"
+        aria-label="웹페이지의 맨위로 이동하기"
+        className="absolute right-4 bottom-[66px]"
+      >
+        <img className="w-12 h-12" src="/images/icons/top-button.svg" alt="" />
+      </button>
+    </div>
   );
 }
 
