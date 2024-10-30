9642712e26167d8dcdc0ca0f2d701c136d9bf1af
DragonTiger92 <dczwtu12b@gmail.com>
2024-10-30 17:13:57 +0900

    [Feat] 앱 초기 접근 시 서재 페이지에서 필요한 data를 prefetch하는 코드 작성

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,8 +3,22 @@ import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import SplashPage from '../components/pages/SplashPage/SplashPage';
 import GlobalNavigator from '/src/components/organisms/GlobalNavigator/GlobalNavigator';
 import { loginUserData } from '../utils/controlUserData';
+import { useQueryClient } from '@tanstack/react-query';
+import { getAllUserLibRecs } from '../utils/controlBookData';
 
 function App() {
+  // 서재 페이지에 필요한 데이터 prefetch
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (loginUserData)
+      queryClient.prefetchQuery({
+        queryKey: ['library', loginUserData],
+        queryFn: getAllUserLibRecs,
+      });
+  }, [queryClient]);
+
+  // 앱 접근 시 사용자에게 보여줄 컴포넌트 결정 및 isSplashed 상태 변수 관리
   const { pathname } = useLocation();
   const navigate = useNavigate();
   const [isSplashed, setIsSplashed] = useState(false);
