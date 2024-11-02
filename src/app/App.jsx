694be3a639981f49c876bbf1f89fb0992ad8cd82
DragonTiger92 <dczwtu12b@gmail.com>
2024-11-02 17:03:52 +0900

    [Comment] App.jsx 내 특정 영역 코드에 대한 주석 종류를 블록 주석으로 변경, 주석 설명에 isInitialized에 관한 내용 추가

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,7 +7,7 @@ import { useQueryClient } from '@tanstack/react-query';
 import { getAllUserLibRecs } from '../utils/controlBookData';
 
 function App() {
-  // 서재 페이지에 필요한 데이터 prefetch
+  /* 서재 페이지에 필요한 데이터 prefetch */
   const queryClient = useQueryClient();
 
   useEffect(() => {
@@ -18,7 +18,7 @@ function App() {
       });
   }, [queryClient]);
 
-  // 앱 접근 시 로그인 여부와 'isSplashed' 상태 변수 값에 따른 action 결정
+  /* 앱 접근 시 로그인 여부와 'isSplashed', 'isInitialized' 상태 변수 값에 따른 action 결정 */
   const [isSplashed, setIsSplashed] = useState(false);
   // 앱의 초기 설정이 전부 다 완료 되었는지 체크하는 상태 변수 선언
   const [isInitialized, setIsInitialized] = useState(false);
@@ -52,7 +52,7 @@ function App() {
     }
   }, [isInitialized, isSplashed, navigateUser]);
 
-  // isSplashed 상태 변수와 URL의 path 값에 따라 App 컴포넌트의 return 값을 다르게 설정
+  /* isSplashed, isInitialized 상태 변수와 URL의 path 값에 따라 App 컴포넌트의 return 값을 다르게 설정 */
   const { pathname } = useLocation();
 
   if (!isInitialized) return null;
