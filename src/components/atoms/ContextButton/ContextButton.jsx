d77ab11a944f47f4714f556c1d337060faf71398
juhyuny <juhyun5358@gmail.com>
2024-11-03 17:16:42 +0900

    [Modify] ContextButton 수정 및 구현
    - 컴포넌트 이름 수정
    - 버튼 유형 prop(vertical, horizontal)
    - aria-label prop
    - customClassName prop 활용

--- src/components/atoms/ContextButton/ContextButton.jsx
+++ src/components/atoms/ContextButton/ContextButton.jsx
@@ -1,4 +1,5 @@
 import { string } from 'prop-types';
+// import getCombinedClassName from '../../../utils/getCombinedClassName';
 
 /**
  *
@@ -6,23 +7,29 @@ import { string } from 'prop-types';
  * @param { string } alt 컨텍스트 버튼의 alt message
  * @returns { JSX.Element } Context button이 포함된 div 요소 반환
  */
-function ContextButton({ shape, alt }) {
+function ContextButton({ shape, ariaLabel, customClassName = '' }) {
+  const defaultClassName =
+    'w-11 h-11 flex justify-center items-center absolute z-10';
+
+  // 문제 가능성 이슈로 잠시 사용보류
+  // const combinedClassName = getCombinedClassName(
+  //   defaultClassName,
+  //   customClassName
+  // );
+
+  const ClassName = `${defaultClassName} ${customClassName}`;
+
   return (
-    <div>
-      <button type="button">
-        <img
-          className="w-9 h-9"
-          src={`/images/icons/more_${shape}.svg`}
-          alt={alt}
-        />
-      </button>
-    </div>
+    <button type="button" className={ClassName} aria-label={ariaLabel}>
+      <img className="w-6 h-6" src={`/images/icons/more_${shape}.svg`} alt="" />
+    </button>
   );
 }
 
 ContextButton.propTypes = {
   shape: string.isRequired,
-  alt: string.isRequired,
+  ariaLabel: string.isRequired,
+  customClassName: string,
 };
 
 export default ContextButton;
