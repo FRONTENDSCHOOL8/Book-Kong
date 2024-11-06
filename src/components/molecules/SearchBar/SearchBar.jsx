4b04ecb7b0e8cb34866793cc4c74581b5f8b08c0
DragonTiger92 <dczwtu12b@gmail.com>
2024-11-07 01:14:36 +0900

    [Feat] SearchBar 내 SearchInput의 query 존재 여부에 따라 ResetBtn 컴포넌트 렌더링 여부를 결정하는 로직 작성

--- src/components/molecules/SearchBar/SearchBar.jsx
+++ src/components/molecules/SearchBar/SearchBar.jsx
@@ -8,7 +8,7 @@ function SearchBar({ query, onQueryChange, onResetClick, pgName }) {
     <form className="bg-primary-50 w-full flex justify-between items-center gap-2 rounded border border-primary-500 px-3 py-2 ">
       <SearchIcon />
       <SearchInput query={query} onChange={onQueryChange} pgName={pgName} />
-      <ResetBtn onClick={onResetClick} />
+      {query === '' ? null : <ResetBtn onClick={onResetClick} />}
     </form>
   );
 }
