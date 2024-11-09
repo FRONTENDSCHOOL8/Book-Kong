593e2d954071748d0ffd7d7f55e14e94bcadcf53
juhyuny <juhyun5358@gmail.com>
2024-11-09 11:19:50 +0900

    [Design] FeedDetailPage 내 레이아웃 스타일 수정, Input 스타일 수정

--- src/components/molecules/FeedDetailPostContents/FeedDetailPostContents.jsx
+++ src/components/molecules/FeedDetailPostContents/FeedDetailPostContents.jsx
@@ -2,12 +2,12 @@ import {} from 'prop-types';
 
 function FeedDetailPostContents() {
   return (
-    <section className="mb-3 mt-[69px] bg-grayscale-white border-grayscale-200 border px-4">
-      <p className="py-6 text-grayscale-500 text-content-xs text-right">
+    <section className="px-4 mb-3 border-t border-b bg-grayscale-white border-grayscale-200">
+      <p className="py-6 text-right text-grayscale-500 text-content-xs">
         2024년 3월 1일
       </p>
       <h3 className="contents-xs text-primary-500">역행자(확장판)</h3>
-      <h2 className="contents-lg py-4 text-grayscale-900">역행자 읽었어용</h2>
+      <h2 className="py-4 contents-lg text-grayscale-900">역행자 읽었어용</h2>
       <p className="contents-sm pb-[86px] text-grayscale-black">
         역행자를 읽었는데 제가 역행할 수 있을까요?
       </p>
